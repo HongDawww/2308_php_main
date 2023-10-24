d0afa12c7851b1ec5f4413e2fd7fcf4e4e04ade5
HongDawww <d939305@naver.com>
2023-10-24 11:37:54 +0900

    1024 js

--- main/javascript/js/04.js
+++ main/javascript/js/04.js
@@ -17,3 +17,61 @@ console.log(uu_name);
 // const : 중복 선언 불가능 , 재할당 불가능 , 블록 레벨 스코프
 const AGE = 19;
 console.log(AGE);
+
+
+// ------------------------ 전역 스코프 --------------------------
+
+let gender = "M";
+
+//-------------------함수 레벨 스코프 (지역 스코프) --------------------------
+
+function test() {
+    let test1 = "test1";
+    console.log(test1);
+    console.log(gender);
+}
+
+//----------------- 블록 레벨 스코프 (지역 스코프) --------------------------
+
+function test2() {
+    var t = "test1";
+    if(true) {
+        var t = "test2";
+    }
+    console.log(t);
+}
+
+function test3() {
+    let t = "test1";
+    if(true) {
+        let t = "test2";
+    }
+    console.log(t);
+}
+
+
+//----------------- 호이스팅 (hoisting) -------------------------------------------
+// 인터프리터가 변수와 함수의 메모리 공간을 선언 전에 미리 할당하는 것
+// 인터프리터 : 프로그래밍 언어의 소스 코드를 바로 실행하는 컴퓨터 프로그램 또는 환경
+// 코드가 실행되기 전에 변수와 함수를 최상단에 끌어 올려지는 것
+
+console.log(htest1());
+
+function htest1() {
+    return "htest1 함수입니다";
+}
+
+
+// console.log(hvar()); // undifined 
+
+//호이스팅의 기준이 달라짐 
+var hvar = "var로 선언";
+let hlet = "let으로 선언";
+
+// console.log(hvar());  
+// console.log(hler());
+
+console.log(hvar);
+console.log(hlet);
+
+// ---------------------------------------------------------------------------------
---
+++ main/javascript/js/06.js
@@ -0,0 +1,49 @@
+// 기본 데이터 타입
+
+// 숫자( number ) , 문자열 ( string ), boolean , null , undifined , symbol
+
+let num = 1;
+
+let str = "문자열";
+
+let boo = true;
+
+let nu = null; // object (객체) 로 인식
+
+let und;
+
+
+// 고유한 id를 가진 데이터 타입
+// let symbol_1 = symbol("symbol");
+// let symbol_2 = symbol("symbol");
+
+// 객체 타입
+// Array , Date , Math , Object , 그 외에 기본 데이터 타입을 제외한 모든 것
+
+let obj = {
+    food1: "탕수육"
+    ,food2: "짜장면"
+    ,food3: "라조기"
+    ,eat: function() {
+        console.log("먹자");
+    }
+    ,list: {
+        list1: "리스트1"
+        ,list2: "리스트2"
+    }
+};
+
+
+let arr = [1,2,3];
+
+// 자기 자신의 회원 정보를 객체로 만들기
+
+let hong = {
+    first_name: "다윗"
+    ,age: 31
+    ,address: "daegu"
+    ,id: function(){
+        console.log("d939305@naver.com");
+    }
+   
+};
