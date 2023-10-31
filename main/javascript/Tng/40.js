209f740a48cb04a24dca9c91154afe5abf9713f3
HongDawww <d939305@naver.com>
2023-10-31 09:25:59 +0900

    1031

--- main/javascript/Tng/40.js
+++ main/javascript/Tng/40.js
@@ -98,3 +98,136 @@
 
 
 
+// 하
+// 1. 버튼을 클릭시 아래 내용의 알러트가 나옵니다.
+	//"안녕하세요."
+	//"숨어있는 div를 찾아보세요."
+
+// 중하
+//2-1. 특정 영역에 마우스 포인터가 진입하면 아래 내용의 알러트가 나옵니다.
+	//"두근두근"
+
+// 상
+//2-2. 들킨 상태에서는 알러트가 안나옵니다. 안 들켰으면 계속 알러트가 나와야 합니다.
+
+// 중하
+//3. 2번의 영역을 클릭하면 아래의 알러트를 출력하고, 배경색이 베이지 색으로 바뀌어 나타납니다.
+	//"들켰다!"
+
+// 상
+//4. 3번의 상태에서 다시 한번더 클릭하면 아래의 알러트를 출력하고, 배경색이 흰색으로 바뀌어 안보이게 됩니다.
+	//"다시 숨는다."
+
+
+//   1  >>  2-1  >> 3  >> 4  >> 2-2
+
+// --------------
+// mk 소스코드
+// --------------
+// const BTN = document.getElementById('BTN');
+// BTN.addEventListener('click', () => {
+// 	alert('안녕하세요'+'\n'+'숨어있는 div를 찾아보슈')
+// 	BTN.classList.add('start');
+// });
+
+// const DIV1 = document.getElementById('div1');
+// DIV1.addEventListener('mouseenter', () => {
+// 	if(getComputedStyle(DIV1).backgroundColor === 'rgb(255, 255, 255)' && BTN.className === 'start') {
+// 		alert('두근두근')
+// 	}
+// });
+
+// DIV1.addEventListener('click', () => {
+// 	if(getComputedStyle(DIV1).backgroundColor === 'rgb(255, 255, 255)' && BTN.className === 'start'){
+// 		alert('들켰당');
+// 		DIV1.style.backgroundColor = 'beige';
+// 	} else if(getComputedStyle(DIV1).backgroundColor === 'rgb(245, 245, 220)' && BTN.className === 'start') {
+// 		alert('다시 숨는다');
+// 		DIV1.style.backgroundColor = 'white';
+// 		let randX = Math.ceil(Math.random() * window.innerWidth);
+// 		let randY = Math.ceil(Math.random() * window.innerHeight);
+// 		DIV1.style.right = randX+'px';
+// 		DIV1.style.bottom = randY+'px';
+// 	}
+// });
+
+
+
+
+// --------------
+// 강사
+// --------------
+const BTN = document.querySelector('#btn');
+BTN.onclick = function(){
+	alert('안녕하세요\n숨어있는 div를 찾아보세요');
+}
+
+const WRAP = document.querySelector('#wrap');
+
+function popUp(){
+	alert('두근두근');
+}
+
+WRAP.addEventListener('mouseenter',popUp);
+
+const BOX = document.querySelector('#box');
+
+BOX.addEventListener('click', my_toggle);
+
+function my_toggle(){
+	if( BOX.className === "on"){
+		BOX.classList.toggle("on");
+		alert("다시숨는다");
+		WRAP.addEventListener("mouseenter",popUp);
+	} else {
+		BOX.classList.toggle("on");
+		alert("들켰다!");
+		WRAP.removeEventListener('mouseenter',popUp);
+	} 
+}
+
+// <!-- mk 소스코드 -->
+// <!-- <!DOCTYPE html>
+// <html lang="ko">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <style>
+//         #div1 {
+//             width: 200px;
+//             height: 200px;
+//             position: absolute;
+//             right: 578px;
+//             bottom: 334px;
+//             background-color: white;
+//         }
+//     </style>
+// </head>
+// <body>
+//     <button id="btn">눌러보시오</button>
+//     <div id="div1"></div>
+//     <script src="./40.js"></script>
+// </body>
+// </html> -->
+
+
+
+// <!-- 강사 -->
+// <!DOCTYPE html>
+// <html lang="ko">
+// <head>
+// 	<meta charset="UTF-8">
+// 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+// 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 	<link rel="stylesheet" href="./40.css">
+// 	<title>Event Ex1</title>
+// </head>
+// <body>
+// 	<button type="button" id="btn">버튼</button>
+// 	<div id="wrap">
+// 		<div id="box" class=""></div>
+// 	</div>
+// 	<script src="./40.js"></script>
+// </body>
+// </html>
--- main/javascript/Tng/41.js
+++ main/javascript/Tng/41.js
@@ -34,3 +34,66 @@ RESRT.addEventListener("click",function () {
 });
 
 // setInterval(getClock, 1000);
+
+
+// const PRINTTIME = document.getElementById('clock');
+
+// // 하
+// // 1. 현재 시간을 화면에 표시
+
+// // 중
+// // 2. 실시간으로 시간을 화면에 표시
+
+// let interval;
+// startTime();
+
+// function getNow() {
+// 	const NOW = new Date();
+// 	const NOWUSA = new Date(); // 미국시간 구할 Date
+// 	let hour_24 = NOW.getHours();
+// 	let minute = NOW.getMinutes();
+// 	let second = NOW.getSeconds();
+// 	let AMPM = hour_24 > 12 ? '오후' : '오전';
+// 	let hour_12 = hour_24 > 12 ? hour_24 - 12 : hour_24;
+// 	let print_time = 
+// 		AMPM + ' '
+// 		+ add_str_zero(hour_12) + ':'
+// 		+ add_str_zero(minute) + ':'
+// 		+ add_str_zero(second);
+
+// 	NOWUSA.setTime( NOW - (1000 * 60 * 60 * 13) ); // 현재시간 - 13시
+// 	let now_usa = NOWUSA.toLocaleTimeString();
+
+// 	PRINTTIME.innerHTML = print_time + '<br>'  + now_usa;
+
+// 	// --------------
+// 	// 시간을 이렇게도 가져올 수 있습니다.('오후 4:40:21')
+// 	// --------------
+// 	// PRINTTIME.innerHTML = NOW.toLocaleTimeString();
+// }
+
+// // 숫자가 10 미만이면 '0'을 붙여주는 함수
+// function add_str_zero(num) {
+// 	return String(num < 10 ? '0' + num : num); 
+// }
+
+
+// // 중하
+// // 3. 멈춰 버튼을 누르면, 시간이 정지할 것
+// const BTNSTOP = document.querySelector('#btn-stop');
+// BTNSTOP.addEventListener('click', stopTime);
+
+// function stopTime() {
+// 	clearInterval(interval);
+// }
+
+// // 중상
+// // 4. 재시작 버튼을 누르면, 버튼을 누른 시점의 시간부터 다시 실시간으로 화면에 표시
+// const BTNRESTART = document.querySelector('#btn-restart');
+// BTNRESTART.addEventListener('click', startTime);
+
+// function startTime() {
+// 	getNow(); // 시작 or 재시작시 1초딜레이 생기는 현상 없애기 위해 추가
+// 	interval = setInterval( getNow, 1000 );
+// }
+
