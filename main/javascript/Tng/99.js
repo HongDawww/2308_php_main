686c6577c4abdb3337c2cb4eeaa007d7bb1fcf88
HongDawww <d939305@naver.com>
2023-11-01 09:35:45 +0900

    1101

--- main/javascript/Tng/99.js
+++ main/javascript/Tng/99.js
@@ -1,80 +1,11 @@
-
-//   const BTN_API = document.querySelector('#btn-api'); // 버튼 객체 가져옴
-//   BTN_API.addEventListener('click', my_fetch);
-  
-//   function my_fetch() {
-//       const INPUT_URL = document.querySelector('#input-url');
-  
-//       fetch(INPUT_URL.value.trim())
-//       .then( response => {
-//           if( response.status >= 200 && response.status < 300) {
-//               return response.json();
-//           } else {
-//               throw new Error('에러');
-//           }
-//       })
-//       .then ( data => makeImg(data) )
-//       .catch( error => console.log(error));
-//   }
-  
-//   const DEL_BTN = document.getElementById("del-btn");
-//   DEL_BTN.addEventListener('click', apiDelete);
-  
-//   function apiDelete() {
-//       const DIV_IMG = document.querySelector('#div-img');
-//       const DEL_API = DIV_IMG.querySelectorAll('.image-wrap'); 
-//       DEL_API.forEach(DEL_API => {
-//           DEL_API.remove();
-//       });
-//   }
-  
-//   function makeImg(data) {
-
-//         data.forEach(item => {
-//         const NEW_ID = document.createElement('p');
-//         NEW_ID.style.display ="flex";
-//         NEW_ID.style.textAlign = 'center';
-
-//         const DIV_IMG = document.querySelector('#div-img');
-//         DIV_IMG.style.display = 'grid';
-//         DIV_IMG.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
-//         // DIV_IMG.style.gap = '1px';
-  
-
-//         const IMAGE_WRAP = document.createElement('div'); 
-//         IMAGE_WRAP.className = 'image-wrap'; 
-  
-
-//         const IMAGE_DIV = document.createElement('div');
-
-
-//         const NEW_IMG = document.createElement('img');
-
-//         NEW_IMG.setAttribute('src', item.download_url);
-//         NEW_IMG.style.width = '200px';
-//         NEW_IMG.style.height = '200px';
-
-//         NEW_ID.innerText= `${item.id}`;
-
-//         IMAGE_DIV.appendChild(NEW_ID);
-//         IMAGE_DIV.appendChild(NEW_IMG);
-        
-//         IMAGE_WRAP.appendChild(IMAGE_DIV); 
-
-//         DIV_IMG.appendChild(IMAGE_WRAP);
-//       });
-//   }
-  
-  // makeImg(data)
-
-
 const BTN_API = document.querySelector('#btn-api');
 BTN_API.addEventListener('click', my_fetch);
 
 function my_fetch() {
-    const INPUT_URL = document.querySelector('#input-url');
+    const INPUT_URL = document.querySelector('#nav-input');
+    const url = INPUT_URL.value;
 
-    fetch(INPUT_URL.value.trim())
+    fetch(url)
     .then(response => {
         if (response.status >= 200 && response.status < 300) {
             return response.json();
@@ -86,51 +17,34 @@ function my_fetch() {
     .catch(error => console.log(error));
 }
 
-const DEL_BTN = document.getElementById("del-btn");
+const DEL_BTN = document.getElementById("btn-clear");
 DEL_BTN.addEventListener('click', apiDelete);
 
 function apiDelete() {
     const DIV_IMG = document.querySelector('#div-img');
-    const DEL_API = DIV_IMG.querySelectorAll('main');
-    DEL_API.forEach(DEL_API => {
-        DEL_API.remove();
-    });
+    while (DIV_IMG.firstChild) {
+        DIV_IMG.firstChild.remove();
+    }
 }
 
 function makeImg(data) {
+    const DIV_IMG = document.querySelector('#div-img');
+
     data.forEach(item => {
-        const NEW_ID = document.createElement('div');
-        // NEW_ID.style.display = "flex";
-        // NEW_ID.style.textAlign = 'center';
-
-        const DIV_IMG = document.querySelector('#div-img');
-        
-        DIV_IMG.style.display = 'grid';
-        DIV_IMG.style.gridTemplateColumns = 'repeat(4, minmax(250px, 1fr))';
-        DIV_IMG.style.gridGap = '10px';
-
         const IMAGE_WRAP = document.createElement('div');
-        IMAGE_WRAP.className = 'main';
-        IMAGE_WRAP.style.background = 'gray';
+        IMAGE_WRAP.className = 'image-div';
 
-        const IMAGE_DIV = document.createElement('div');
-
-        
         const NEW_IMG = document.createElement('img');
-        NEW_IMG.setAttribute('src', item.download_url);
-      
-        // NEW_IMG.style.width = '200px';
-        // NEW_IMG.style.height = '200px';
+        NEW_IMG.className = 'image';
+        NEW_IMG.src = item.download_url;
 
-        NEW_ID.innerText = `${item.id}`;
+        const NEW_ID = document.createElement('p');
+        NEW_ID.className = 'image-id';
+        NEW_ID.innerText = `ID: ${item.id}`;
 
-        IMAGE_DIV.appendChild(NEW_ID);
-        IMAGE_DIV.appendChild(NEW_IMG);
-
-        IMAGE_WRAP.appendChild(IMAGE_DIV);
+        IMAGE_WRAP.appendChild(NEW_ID); 
+        IMAGE_WRAP.appendChild(NEW_IMG); 
 
         DIV_IMG.appendChild(IMAGE_WRAP);
     });
 }
-
-// 'repeat(auto-fill, minmax(250px, 1fr))';
