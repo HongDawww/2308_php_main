e038eafcb2e69fc89bc5efff09acf7b725485182
HongDawww <d939305@naver.com>
2023-11-13 17:07:06 +0900

    seeder insert

--- main/larabel-edu/database/seeders/CategorySeeder.php
+++ main/larabel-edu/database/seeders/CategorySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\support\Facades\DB;
 class CategorySeeder extends Seeder
 {
     /**
@@ -14,6 +14,17 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert([
+            ['no' => '0','name' => '카테고리0']
+            ,['no' => '1','name' => '카테고리1']
+            ,['no' => '2','name' => '카테고리2']
+            ,['no' => '3','name' => '카테고리3']
+            ,['no' => '4','name' => '카테고리4']
+            ,['no' => '5','name' => '카테고리5']
+            ,['no' => '6','name' => '카테고리6']
+            ,['no' => '7','name' => '카테고리7']
+            ,['no' => '8','name' => '카테고리8']
+            ,['no' => '9','name' => '카테고리9']
+        ]);
     }
 }
