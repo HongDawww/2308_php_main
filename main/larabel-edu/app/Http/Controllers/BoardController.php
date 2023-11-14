a7f3d21877925e5e0589115d44c4858c49ac05d7
HongDawww <d939305@naver.com>
2023-11-14 12:56:48 +0900

    sql

--- main/larabel-edu/app/Http/Controllers/BoardController.php
+++ main/larabel-edu/app/Http/Controllers/BoardController.php
@@ -11,6 +11,55 @@ class BoardController extends Controller
 
     $result = DB::select('select * from boards limit 10');
 
+    $result = DB::select('select * from boards limit :no', ['no' => 1]);
+    // $result = DB::select('select * from boards limit ?', [2]);
+    $result = DB::select('select * from boards limit ? offset ?', [2, 10]);
+    
+    $arr = [4,7,8];
+    $result = DB::select(
+         'select count(id)
+            from boards 
+            where categories_no = ? 
+            or categories_no = ? 
+            or categories_no = ?'
+        , $arr);
+
+    $result = DB::select('select count(categories_no) as cnt from boards group by categories_no');
+    
+    $result = DB::select(
+         'SELECT 
+            ca.no
+            ,ca.name
+            ,COUNT(ca.no) AS cnt
+        FROM boards bo
+               JOIN  categories ca
+                ON bo.categories_no = ca.no
+        GROUP BY ca.no, ca.name'
+        );
+  
+    $sql = 
+            'insert into boards(title, content, created_at, updated_at, categories_no) values (?,?,?,?,?)';
+    $arr = [
+                '제목1'
+                ,'내용1'
+                ,now()
+                ,now()
+                ,'0'
+    ];
+    // DB::begintransaction();
+    // DB::insert($sql, $arr);
+    // DB::commit();
+   
+    $result = DB::select('select * from boards order by id desc limit 1');
+    // DB::begintransaction();
+    // DB::update('update boards set title = ?, content = ? where id = ?',['업데이트1','업데이트',$result[0]->id]);
+    // DB::commit();
+
+    // DB::beginTransaction();
+    // DB::delete('delete from boards where id = ?' ,[$result[0]->id]);
+    // DB::commit();
+
+
     return var_dump($result);
    }
 }
