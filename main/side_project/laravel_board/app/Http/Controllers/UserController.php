4a6017fd89f6ac764639c1d7359acab7d5b214ce
HongDawww <d939305@naver.com>
2023-11-15 12:10:16 +0900

    1115 회원가입

--- main/side_project/laravel_board/app/Http/Controllers/UserController.php
+++ main/side_project/laravel_board/app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\user;
 
 class UserController extends Controller
 {
@@ -15,7 +18,26 @@ class UserController extends Controller
     public function registrationget(){
         return view('registration');
     }
-    public function registrationpost(){
-        return '회원가입 포스트';
+    public function registrationpost(Request $request){
+
+        $validator = Validator::make(
+            $request->only('name','email','password','passwordchk')
+            ,[
+                'email'     => 'required|email|max:50'
+                ,'name'     => 'required|regex:/^[a-zA-Z가-힣]+$/|min:2|max:50'
+                ,'password' => 'required|same:passwordchk'
+            ]
+        );
+        if($validator->fails()){
+            return view('registration')->withErrors($validator->errors());
+        }
+
+        $data = $request->only('name','email','password');
+
+        $data['password'] = Hash::make($data['password']);
+
+        $result = User::create($data);
+
+        return redirect()->route('user.login.get');
     }
 }
--- main/side_project/laravel_board/resources/views/login.blade.php
+++ main/side_project/laravel_board/resources/views/login.blade.php
@@ -1,7 +1,19 @@
 @extends('layout.layout')
 @section('title','login')
 @section('main')
-    <main>
-        <h1>Login</h1>
-    </main>
+<main class="d-flex justify-content-center align-items-center h-75">
+		<form style="width: 300px" action="{{route('user.login.post')}}" method="post">
+			@csrf
+			<div id="errorMsg" class="form-text text-danger"></div>
+			<div class="mb-3">
+				<label for="email" class="form-label">이메일</label>
+				<input type="text" class="form-control" id="email">
+			</div>
+			<div class="mb-3">
+				<label for="password" class="form-label">비밀번호</label>
+				<input type="password" class="form-control" id="password">
+			</div>
+			<button type="submit" class="btn btn-dark">로그인</button>
+		</form>
+</main>
 @endsection
